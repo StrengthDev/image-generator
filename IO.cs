baa77518100e61c10d69e8a6469fa26c26eeaa50
Pedro Valente <overyourcomprehension@gmail.com>
2021-05-13 04:41:30 +0100

    Image conversion to NumSharp structs

---
+++ IO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using NumSharp;
+
+namespace image_generator
+{
+    class IO
+    {
+        public static NDArray ImageToMatrix(Bitmap image)
+        {
+            BitmapData bmpData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, image.PixelFormat);
+            NDArray matrix = new NDArray(NPTypeCode.Byte, Shape.Vector(bmpData.Stride * image.Height), fillZeros: false);
+            try
+            {
+                unsafe
+                {
+                    byte* src = (byte*)bmpData.Scan0;
+                    byte* dst = (byte*)matrix.Unsafe.Address;
+
+                    Buffer.MemoryCopy(src, dst, matrix.size, matrix.size);
+                    return matrix.reshape(1, image.Height, image.Width, 4);
+                }
+            }
+            finally
+            {
+                image.UnlockBits(bmpData);
+            }
+        }
+
+        public static Bitmap MatrixToImage(NDArray data)
+        {
+            int[] dims = data.shape;
+            Bitmap image = new Bitmap(dims[0], dims[1], PixelFormat.Format32bppArgb);
+            BitmapData bmpData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.WriteOnly, image.PixelFormat);
+            NDArray tmp = new NDArray(NPTypeCode.Byte, Shape.Vector(bmpData.Stride * image.Height), fillZeros: true);
+            tmp += data.flatten();
+            try
+            {
+                unsafe
+                {
+                    byte* src = (byte*)tmp.Unsafe.Address;
+                    byte* dst = (byte*)bmpData.Scan0;
+
+                    Buffer.MemoryCopy(src, dst, data.size, data.size);
+                    return image;
+                }
+            }
+            finally
+            {
+                image.UnlockBits(bmpData);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Tensorflow.Binding;
+using static Tensorflow.KerasApi;
+using Tensorflow;
+using NumSharp;
+using System.Drawing;
 
 namespace image_generator
 {
@@ -10,6 +15,16 @@ namespace image_generator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Training folder path:");
+            string trainPath = Console.ReadLine();
+
+            //var ((x_train, y_train), (x_test, y_test)) = keras.datasets.cifar10.load_data();
+
+            NDArray img = IO.ImageToMatrix(new Bitmap(@"Src.png"));
+
+            Bitmap n = IO.MatrixToImage(img[0]);
+            n.Save("Test.png");
+
         }
     }
 }
